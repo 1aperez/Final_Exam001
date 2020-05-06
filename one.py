41eb641926794621e940e2bc46cce1b858887936
1aperez <42749606+1aperez@users.noreply.github.com>
2020-05-05 19:22:42 -0500

    Update one.py

--- one.py
+++ one.py
@@ -2,14 +2,27 @@
     For this first problem fix the function
     so that if the input text is a pallendrome, it returns True.
     And if not, it returns false.
-
     Try running it on some samples to make sure it works
 """
 
+def reverse(text):
+    return text[::-1]
+
+def remove(text):
+    return text.replace(" ", "")
+
+def lower(text):
+    return text.lower()
 
 def checkisPallendrome(text):
-    result = False
-    return result
+    undercase = lower(text)
+    result = remove(undercase)
+    backwards = reverse(result)
+    if (result == backwards):
+        x = True
+    else:
+        x = False
+    return x
 
 
 def tester(text, expected_result):
@@ -30,8 +43,12 @@ if __name__ == "__main__":
     text2 = "She sells sea shells by the sea shore"
     text3 = "race car"
     text4 = "My school"
-
+    
+    
+    
     # Test it like this
     print(tester(text0, False))
     print(tester(text1, True))
+    print(tester(text2, False))
+    print(tester(text3, True))
     print(tester(text4, False))
